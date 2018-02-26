23ec850c31793b3492bb164e167e73c081f6d73f
Dimitrij <kvarkas@gmail.com>
2018-02-26 10:46:06 +0000

    init
    first commit

---
+++ NewGUI.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Time sheet beautifier
+// @namespace    https://github.com/Kvarkas
+// @version      0.1
+// @description  To make SAP generated GUI more human frendly 
+// @author       Kvark
+// @match        https://www.myatos.net/irj/servlet/prt/portal/prtroot/pcd!3aportal_content!2fnet.atos.MyAtos!2fTechnicalContent!2fPortalDisplay!2fnet.atos.tec.iv.FrameworkContentArea?*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+(function() {
+    'use strict';
+    function Test() {
+        alert('Hello world!');
+    }
+})();
